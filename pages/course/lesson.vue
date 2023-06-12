0d3b711f1de83b3d4e579c7adbcb0bcb1290de87
vlad polevoi <vlad.polewoi1@gmail.com>
2023-06-13 00:26:54 +0300

    course page

---
+++ pages/course/lesson.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    Lesson 
+  </div>
+</template>
+
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+	<h3>Hello from index</h3>
+</template>
+
+<script>
+export default {
+	name: "index",
+	props: {},
+}
+</script>
+
+<style lang="scss" scoped></style>
