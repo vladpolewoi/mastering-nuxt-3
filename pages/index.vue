cd4ef0a81abb8a220e94c3b405fef2e0bf63e5bc
vlad polevoi <vlad.polewoi1@gmail.com>
2023-07-11 20:11:58 +0300

    feat: :sparkles: link to course

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,13 @@
 <template>
-	<h3>Hello from index</h3>
+	<div
+		class="p-12 bg-gray-100 w-full h-full min-h-screen flex flex-col items-center"
+	>
+		<NuxtLink
+			class="border border-green-500 rounded-md p-4 text-green-500"
+			to="/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3"
+			>Go to course</NuxtLink
+		>
+	</div>
 </template>
 
 <script>
