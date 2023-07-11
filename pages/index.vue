a8417eccd4a030f876d498c634b34837580c3d4f
vlad polevoi <vlad.polewoi1@gmail.com>
2023-07-11 23:48:59 +0300

    feat: :sparkles: add TS to useCourse composab;e

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,9 @@
 	<NuxtLink
 		class="border border-green-500 rounded-md p-4 text-green-500"
 		to="/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3"
-		>Go to course</NuxtLink
 	>
+		Go to course
+	</NuxtLink>
 </template>
 
 <script>
