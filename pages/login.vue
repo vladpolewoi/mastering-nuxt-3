7091c12224341c5d325b9caea19f8c0735b34aca
Vlad Polevoi <vlad.polewoi1@gmail.com>
2023-07-26 23:26:18 +0300

    feat: :sparkles: github login + userCard component

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,10 @@
 <template>
 	<div class="prose w-full max-w-2xl h-9">
 		<h1>Log in to {{ title }}</h1>
-		<button class="bg-blue-500 text-white font-bold py-2 px-4 rounded">
+		<button
+			class="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+			@click="handleLogin"
+		>
 			Log in with Github
 		</button>
 	</div>
@@ -9,4 +12,15 @@
 
 <script setup lang="ts">
 const { title } = useCourse()
+const supabase = useSupabaseClient()
+
+async function handleLogin() {
+	const { error } = await supabase.auth.signInWithOAuth({
+		provider: "github",
+	})
+
+	if (error) {
+		console.log("Error Github Login: ", error)
+	}
+}
 </script>
