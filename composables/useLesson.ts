8caf0f97df8d4399007872a2ae8d1519a2b7e6fd
vlad polevoi <vlad.polewoi1@gmail.com>
2023-08-02 20:46:34 +0300

    feat: :sparkles: generic fetchWithCache

--- composables/useLesson.ts
+++ composables/useLesson.ts
@@ -1,29 +1,6 @@
-import { StorageSerializers } from "@vueuse/core"
 import { LessonWithPath } from "types/course"
 
-export default async (chapterSlug: string, lessonSlug: string) => {
-	// Use sessionStorage to cache the lesson data
-	const url = `/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`
-	const lesson = useSessionStorage<LessonWithPath>(url, null, {
-		serializer: StorageSerializers.object,
-	})
-
-	if (!lesson.value) {
-		const { data, error } = await useFetch(
-			`/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`
-		)
-
-		if (error.value) {
-			throw createError({
-				...error.value,
-				statusMessage: `Could not fetch lesson ${lessonSlug} from chapter ${chapterSlug}`,
-			})
-		}
-
-		lesson.value = data.value
-	} else {
-		console.log("GOT from cache", lessonSlug)
-	}
-
-	return lesson
-}
+export default async (chapterSlug: string, lessonSlug: string) =>
+	useFetchWithCache<LessonWithPath>(
+		`/api/course/chapter/${chapterSlug}/lesson/${lessonSlug}`
+	)
