d8be1729b49832ab71823e9c319f80f20572e44c
vlad polevoi <vlad.polewoi1@gmail.com>
2023-08-18 19:36:36 +0300

    fix: :bug: fix meta endpoint bug where no path on lessons

--- server/api/course/meta.get.ts
+++ server/api/course/meta.get.ts
@@ -10,7 +10,9 @@ const lessonSelect = Prisma.validator<Prisma.LessonDefaultArgs>()({
 	},
 })
 
-export type LessonOutline = Prisma.LessonGetPayload<typeof lessonSelect>
+export type LessonOutline = Prisma.LessonGetPayload<typeof lessonSelect> & {
+	path: string
+}
 
 const chapterSelect = Prisma.validator<Prisma.ChapterDefaultArgs>()({
 	select: {
@@ -21,7 +23,12 @@ const chapterSelect = Prisma.validator<Prisma.ChapterDefaultArgs>()({
 	},
 })
 
-export type ChapterOutline = Prisma.ChapterGetPayload<typeof chapterSelect>
+export type ChapterOutline = Omit<
+	Prisma.ChapterGetPayload<typeof chapterSelect>,
+	"lessons"
+> & {
+	lessons: LessonOutline[]
+}
 
 const courseSelect = Prisma.validator<Prisma.CourseDefaultArgs>()({
 	select: {
@@ -30,8 +37,39 @@ const courseSelect = Prisma.validator<Prisma.CourseDefaultArgs>()({
 	},
 })
 
-export type CourseOutline = Prisma.CourseGetPayload<typeof courseSelect>
+export type CourseOutline = Omit<
+	Prisma.CourseGetPayload<typeof courseSelect>,
+	"chapters"
+> & {
+	chapters: ChapterOutline[]
+}
 
-export default defineEventHandler((event) => {
-	return prisma.course.findFirst(courseSelect)
+export default defineEventHandler(async (): Promise<CourseOutline> => {
+	const outline = await prisma.course.findFirst(courseSelect)
+
+	// Error if there is no course
+	if (!outline) {
+		throw createError({
+			statusCode: 404,
+			statusMessage: "Course not found",
+		})
+	}
+
+	// Map the outline so we can add a path to each lesson
+	const chapters = outline.chapters.map((chapter) => {
+		const lessons = chapter.lessons.map((lesson) => ({
+			...lesson,
+			path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+		}))
+
+		return {
+			...chapter,
+			lessons,
+		}
+	})
+
+	return {
+		...outline,
+		chapters,
+	}
 })
