6b93df09777ec6c9f99c20881dc74733ad0f72c3
vlad polevoi <vlad.polewoi1@gmail.com>
2023-09-03 23:55:38 +0300

    feat: :sparkles: landing page + update nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vsharp from "vite-plugin-vsharp"
 
 export default defineNuxtConfig({
+	nitro: {
+		prerender: {
+			routes: ["/landing"],
+		},
+	},
 	devtools: { enabled: true },
+	vite: {
+		plugins: [vsharp()],
+	},
 	modules: [
 		"@nuxtjs/tailwindcss",
 		"@vueuse/nuxt",
--- pages/landing.vue
+++ pages/landing.vue
@@ -68,6 +68,12 @@
 </template>
 
 <script setup lang="ts">
+import screen1 from "~/assets/images/screen1.png"
+import screen2 from "~/assets/images/screen2.png"
+import screen3 from "~/assets/images/screen3.png"
+import screen4 from "~/assets/images/screen4.png"
+import screen5 from "~/assets/images/screen5.png"
+
 const course = await useCourse()
 const learningOutcomes = [
 	"Hands-On Experience with the Benefits of TypeScript",
@@ -76,13 +82,7 @@ const learningOutcomes = [
 	"How to use TypeScript with the Options API",
 	"How to type reactive data, refs, props, custom events, event handlers, DOM elements, template refs, data from provide/inject, and more",
 ]
-const screenshots = [
-	"/images/screen1.png",
-	"/images/screen2.png",
-	"/images/screen3.png",
-	"/images/screen4.png",
-	"/images/screen5.png",
-]
+const screenshots = [screen1, screen2, screen3, screen4, screen5]
 definePageMeta({
 	layout: false,
 })
